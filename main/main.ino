74c1a80fc0706a1b44709bfe1a00b22564be6d31
OctavioTry <sploevis@gmail.com>
2024-06-23 11:49:19 -0600

    jaja

--- main/main.ino
+++ main/main.ino
@@ -15,12 +15,7 @@
 #define LF_HEIGHT 32
 #define LB_WIDTH 32
 #define LB_HEIGHT 32
-// nooooo jaja xddd
-// nose mis  momos nose xd
-// nose mis  momos aaaaaaaaa
-//jajaa
-// nose mis  momos
-//jajaa xddddddd
+
 Adafruit_SSD1306 display(ANCHO_PANTALLA, ALTO_PANTALLA, &Wire, OLED_RESET);
 
 const unsigned char PROGMEM lf []  = {
